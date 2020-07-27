e41851628c10fbf4636b5c5f1bddc5c26e74f8a3
VladimirMaltsev <vov.maltsev@gmail.com>
2020-07-27 15:49:17 +0300

    reinit arspeed sensor

--- src/drivers/differential_pressure/sdp3x/SDP3X.cpp
+++ src/drivers/differential_pressure/sdp3x/SDP3X.cpp
@@ -128,7 +128,7 @@ SDP3X::collect()
 	int ret = transfer(nullptr, 0, &val[0], sizeof(val));
 
 	if (ret != PX4_OK) {
-		//init_sdp3x();
+		init_sdp3x();
 		perf_count(_comms_errors);
 		return ret;
 	}
